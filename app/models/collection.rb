d837ee54cac55ee8b3f481829e823218ae816207
Peter Jang <peter.x.jang@gmail.com>
2019-03-08 20:46:00 +0000

    Add associations

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,2 +1,13 @@
 class Collection < ApplicationRecord
+  belongs_to :collection_category
+  belongs_to :user
+  has_many :records
+
+  def name
+    collection_category.name
+  end
+
+  def description
+    collection_category.description
+  end
 end
--- app/models/collection_category.rb
+++ app/models/collection_category.rb
@@ -1,2 +1,4 @@
 class CollectionCategory < ApplicationRecord
+  belongs_to :user
+  has_many :collections
 end
