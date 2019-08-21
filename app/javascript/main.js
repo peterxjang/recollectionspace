69d5b0db0171a64e53c8e4481e2f07628f068c90
Peter Jang <peter.x.jang@gmail.com>
2019-08-21 16:34:54 +0000

    Add listener for browser back button

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -15,6 +15,14 @@ const Application = {
       onBack: this.handleNavbarBack.bind(this)
     });
     this.loadRouteData();
+    // TODO: Refactor to router and fix double zooming bug
+    window.addEventListener("popstate", evt => {
+      history.go(1);
+      Router.matchUrl("/:username/:collection_name/:id", params => {
+        this.handleNavbarBack();
+      });
+      Canvas.zoomOut();
+    });
   },
   handleNavbarNew: function() {
     if (RecordContent.visible) {
