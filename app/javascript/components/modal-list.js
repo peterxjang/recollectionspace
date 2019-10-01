09f3cbd3d2b6465ff3f8b69d03ad6bcbc595fe3c
Peter Jang <peter.x.jang@gmail.com>
2019-10-01 20:43:46 +0000

    Add sort button functionality

--- app/javascript/components/modal-list.js
+++ app/javascript/components/modal-list.js
@@ -2,6 +2,8 @@ const ModalList = {
   props: null,
   $modal: document.getElementById("modal-list"),
   $links: document.getElementById("modal-list-links"),
+  $buttonSortSize: document.getElementById("modal-list-sort-size"),
+  $buttonSortDate: document.getElementById("modal-list-sort-date"),
   visible: false,
   sortAttribute: "size",
   initialize: function() {
@@ -10,19 +12,34 @@ const ModalList = {
   },
   hide: function() {
     this.$modal.style.display = "none";
+    this.sortAttribute = "size";
     this.visible = false;
   },
   show: function(props) {
     this.props = props;
     this.$modal.style.display = "block";
     this.$modal.scrollTo(0, 0);
+    this.setLinks();
+    this.visible = true;
+  },
+  setLinks: function() {
     this.$links.innerHTML = `<ul>${this.props.items
       .sort((a, b) => b[this.sortAttribute] - a[this.sortAttribute])
       .map(item => `<li><a href="${item.href}">${item.caption}</a></li>`)
       .join("")}</ul>`;
-    this.visible = true;
   },
-  bindEvents: function() {}
+  bindEvents: function() {
+    this.$buttonSortSize.onclick = event => {
+      event.preventDefault();
+      this.sortAttribute = "size";
+      this.setLinks();
+    };
+    this.$buttonSortDate.onclick = event => {
+      event.preventDefault();
+      this.sortAttribute = "date";
+      this.setLinks();
+    };
+  }
 };
 
 export default ModalList.initialize();
